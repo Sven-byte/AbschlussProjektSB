e2606fa75c6d256f364932ed8022651c9b1b87d1
Sven-byte <sven.beilfuss@cgi.com>
2023-04-11 09:56:36 +0200

    iOS Network Changes

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,16 +1,29 @@
 import SwiftUI
 import shared
 
-struct ContentView: View {
-	
 
-	var body: some View {
-		Text("greet")
-	}
+
+struct ContentView: View {
+    @ObservedObject private(set) var viewModel: ViewModel
+
+    var body: some View {
+        Text(viewModel.text)
+    }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+extension ContentView {
+    class ViewModel: ObservableObject {
+        @Published var text = "Loading..."
+        init() {
+            NetworkService().login { login, error in
+                DispatchQueue.main.async {
+                    if let login = login {
+                        self.text = login.username
+                    } else {
+                        self.text = error?.localizedDescription ?? "error"
+                    }
+                }
+            }
+        }
+    }
 }
