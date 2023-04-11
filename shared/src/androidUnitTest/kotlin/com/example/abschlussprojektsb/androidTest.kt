c9e98df4abbb253f0eb0d216013188f62ac31d78
Sven-byte <sven.beilfuss@cgi.com>
2023-04-11 09:30:08 +0200

    Test für Android hinzugefügt / uncomiited changes für iOS

--- shared/src/androidUnitTest/kotlin/com/example/abschlussprojektsb/androidTest.kt
+++ shared/src/androidUnitTest/kotlin/com/example/abschlussprojektsb/androidTest.kt
@@ -1,5 +1,6 @@
 package com.example.abschlussprojektsb
 
+import com.example.abschlussprojektsb.network.NetworkService
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -8,5 +9,10 @@ class AndroidGreetingTest {
     @Test
     fun testExample() {
         assertTrue("Check Android is mentioned", LoadingText().greet().contains("Android"))
+    }
+
+    @Test
+    suspend fun testNetworkCall(){
+        assertTrue("Login Test", NetworkService().login().id.isNotBlank() )
     }
 }
